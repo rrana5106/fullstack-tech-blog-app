c3703962ec9a7943a2a857b87961307e853fe840
rrana5106 <rrana5106@hotmail.com>
2026-07-18 21:37:19 +0100

    Move JWT secret to environment config, restore .env.example

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+const secret = process.env.JWT_SECRET;
 const expiration = '2h';
 
 const authMiddleware = (req, res, next) => {
